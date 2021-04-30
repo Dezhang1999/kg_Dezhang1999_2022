50952c1c8f5f724280fb6c47012fecde5a7181a4
Dequan Zhang <1162838174@qq.com>
2021-04-29 17:17:02 -0700

    finish requried function

--- main.go
+++ main.go
@@ -8,11 +8,11 @@ import (
 
 func main() {
 	args := os.Args[1:]
-	int_args := convert_to_int_array(args)
+	int_args := convertToIntegerArray(args)
 	fmt.Println(int_args)
 }
 
-func convert_to_int_array(args []string) []int {
+func convertToIntegerArray(args []string) []int {
 	result := make([]int, len(args))
 	for i := 0; i < len(args); i++ {
 		val, err := strconv.Atoi(args[i])
@@ -23,8 +23,20 @@ func convert_to_int_array(args []string) []int {
 	return result
 }
 
-func array_phonetic(values []int) []string {
+func converToPhonetic(values []int) []string {
 	result := make([]string, len(values))
-
+	conversionFactor := []string{"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"}
+	for i := 0; i < len(values); i++ {
+		currentVal := values[i]
+		current := ""
+		if currentVal != 0 {
+			index := currentVal % 10
+			currentCoversion := conversionFactor[index]
+			current += fmt.Sprintf("%s%s", currentCoversion, current)
+			currentVal /= 10
+		} else {
+			result[i] = conversionFactor[currentVal]
+		}
+	}
 	return result
 }
