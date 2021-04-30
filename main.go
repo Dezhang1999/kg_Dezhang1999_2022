2c437a8c43025c5438737e9d64b0b29710dcdf5f
Dequan Zhang <1162838174@qq.com>
2021-04-29 17:05:21 -0700

    added conversion str to int function

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
 
+func main() {
+	args := os.Args[1:]
+	int_args := convert_to_int_array(args)
+	fmt.Println(int_args)
+}
+
+func convert_to_int_array(args []string) []int {
+	result := make([]int, len(args))
+	for i := 0; i < len(args); i++ {
+		val, err := strconv.Atoi(args[i])
+		if err == nil {
+			result[i] = val
+		}
+	}
+	return result
+}
+
+func array_phonetic(values []int) []string {
+	result := make([]string, len(values))
+
+	return result
 }
